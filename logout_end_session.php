eed3d455591a79c51161cbd26391cbc9581a1734
michaelgmerritt <gideonmerritt@gmail.com>
2018-03-15 20:29:59 -0400

    Add files via upload

---
+++ logout_end_session.php
@@ -0,0 +1,8 @@
+<?php
+	$message = "Thanks for using C.A.R.R.S.!";
+	echo "<script type='text/javascript'>alert('$message');</script>";
+	echo '<script type="text/javascript">
+		     window.location = "login.html"
+		</script>';
+	die();
+?>
