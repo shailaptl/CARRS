e6d1b5618d03d7c8e1657a68fe01aaf6b12d6769
Shaila Patel <shailaptl@ufl.edu>
2018-03-20 16:31:56 -0400

    Stats page button text changes

--- stats.js
+++ stats.js
@@ -3,23 +3,43 @@ $(function () {
 });
 
 function statsToday() {
+	//printing it
+	var print = document.getElementById('timeValue');
+	print.innerHTML = "today:" ;
+	
+	//change result value
 	printValueCarsPassed(5);
 }
 
 function statsWeek() {
+	//printing it
+	var print = document.getElementById('timeValue');
+	print.innerHTML = "this week:";
+	
+	//change result value
 	printValueCarsPassed(10);
 }
 
 function statsMonth() {
+	//printing it
+	var print = document.getElementById('timeValue');
+	print.innerHTML = "this month:";
+	
+	//change result value
 	printValueCarsPassed(15);
 }
 
 function statsYear() {
+	//printing it
+	var print = document.getElementById('timeValue');
+	print.innerHTML = "this year:";
+	
+	//change result value
 	printValueCarsPassed(20);
 }
 
 function printValueCarsPassed(numCars) {
 	//printing it
-	var print = document.getElementById('printHere');
+	var print = document.getElementById('numCarsResult');
 	print.innerHTML = numCars + "<br/>";
 }
